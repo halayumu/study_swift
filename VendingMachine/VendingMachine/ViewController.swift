651be8116791059354d5ed5b1ddde46f0cf9a846
ISHIDA <ishida.ayumu@isoroot.jp>
2023-09-29 21:01:01 +0900

    お金投入ボタンと画像配置

---
+++ VendingMachine/VendingMachine/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VendingMachine
+//
+//  Created by ishida.a on 2023/09/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("hoge")
+    }
+
+
+}
+
